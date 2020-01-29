2569ac849dd7ddde6e0e929acccf3fc5bd4b4eec
MinexAutomation <minex.automation@gmail.com>
2020-01-29 00:52:42 -0800

    Added startup. Added Program-as-service. Ran, and ServiceAction<T>-based service collection configuration works!

--- source/R5T.Rathgormack.Standard.Construction/Code/Program.cs
+++ source/R5T.Rathgormack.Standard.Construction/Code/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Richmond;
+
+
 namespace R5T.Rathgormack.Standard.Construction
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceProvider = ServiceProviderBuilder.NewUseStartup<Startup>();
+
+            var program = serviceProvider.GetRequiredService<Program>();
+
+            program.Run();
+        }
+
+
+        private IHelloWorldMessenger HelloWorldMessenger { get; }
+
+
+        public Program(IHelloWorldMessenger helloWorldMessenger)
+        {
+            this.HelloWorldMessenger = helloWorldMessenger;
+        }
+
+        public void Run()
+        {
+            this.HelloWorldMessenger.SendHelloWorldMessage();
         }
     }
 }
---
+++ source/R5T.Rathgormack.Standard.Construction/Code/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using R5T.Richmond;
+
+
+namespace R5T.Rathgormack.Standard.Construction
+{
+    public class Startup : StartupBase
+    {
+        public Startup(ILogger<Startup> logger)
+            : base(logger)
+        {
+        }
+
+        protected override void ConfigureServicesBody(IServiceCollection services)
+        {
+            services
+                .AddSingleton<Program>()
+                .AddHelloWorldMessenger();
+        }
+    }
+}
